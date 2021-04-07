58416c69465b17876197080a0cc7a6f089bd7725
samazon <letcsam@gmail.com>
2021-04-08 02:12:48 +0500

    9. Our Work Page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+// Images
+import athlete from '../img/athlete-small.png'
+import goodtimes from '../img/goodtimes-small.png'
+import theracer from '../img/theracer-small.png'
 
 const OurWork = () => {
     return (
-        <div>
-            <h1>OurWork</h1>
-        </div>
+        <Work>
+            <Movie>
+                <h2>The Athelete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </Movie>
+        </Work>
     )
 }
 
+const Work = styled.div`
+    min-height: 100vh;
+    padding: 5rem 2rem;
+`
+const Movie = styled.div`
+    color:#fff;
+    h2 {
+        padding: 1rem 0;
+    }
+    .line {
+        height: 0.5rem;
+        background-color: #ccc;
+        margin: 0 0 3rem;
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`
 export default OurWork
