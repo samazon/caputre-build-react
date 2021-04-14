e5257bfd7aa55bc71aaed6306a64e82378d31722
samazon <letcsam@gmail.com>
2021-04-15 02:24:47 +0500

    12. Hero Animation

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,30 +2,29 @@ import React from 'react'
 import home1 from '../img/home1.png'
 import { Hide, Image, StyledAbout, StyledDescription } from '../styles'
 import {motion} from 'framer-motion';
+// Animations
+import { photoAnim, titleAnim, fade } from '../animation'
 
 
 const HeroSection = () => {
-    const titleAnim = {
-        hidden: {opacity: 0},
-        shown: {opacity: 1, transition: {duration: 2}}
-    }
+
     return (
         <StyledAbout>
             <StyledDescription>
                 <Hide>
-                    <motion.h2>We work to make your</motion.h2>
+                    <motion.h2 variants={titleAnim}>We work to make your</motion.h2>
                 </Hide>
                 <Hide>
-                    <h2><span>dreams</span> come</h2>
+                    <motion.h2 variants={titleAnim}><span>dreams</span> come</motion.h2>
                 </Hide>
                 <Hide>
-                    <h2>true.</h2>
+                    <motion.h2 variants={titleAnim}>true.</motion.h2>
                 </Hide>
-                <p className="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, ut dignissimos sapiente nam eaque porro impedit fugiat.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade} className="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, ut dignissimos sapiente nam eaque porro impedit fugiat.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </StyledDescription>
             <Image>
-                <img src={home1} alt="hero img"/>
+                <motion.img variants={photoAnim} src={home1} alt="hero img"/>
             </Image>
         </StyledAbout>
     )
