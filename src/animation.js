2cdc9aa0d86a3a519880243f6a0239f3732aca3f
samazon <letcsam@gmail.com>
2021-04-17 01:54:38 +0500

    13. Our Work Animation

--- src/animation.js
+++ src/animation.js
@@ -45,3 +45,33 @@ export const photoAnim = {
     },
   },
 };
+
+export const slider = {
+  hidden: { x: '-130%', skew: '45deg' },
+  show: {
+    x: '100%',
+    skew: '0deg',
+    transition: { type: 'tween', ease: 'easeOut', duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: {
+    opacity: 1,
+  },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.15,
+      ease: 'easeOut',
+      duration: 1,
+    },
+  },
+};
+
+export const lineAnim = {
+  hidden: {width: '0%'},
+  show: {
+    width: '100%', transition: {duration: 0.5}
+  }
+}
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -9,31 +9,43 @@ import theracer from '../img/theracer-small.png'
 
 // Animation
 import {motion} from 'framer-motion'
-import {pageAnimation} from '../animation'
+import {pageAnimation, fade, photoAnim,lineAnim, slider, sliderContainer} from '../animation'
 
 const OurWork = () => {
     return (
         <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit">
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athelete</h2>
-                <div className="line"></div>
-                <Link to="/work/the-athlete">
-                    <img src={athlete} alt="athlete"/>
-                </Link>
+                <motion.h2 variants={fade}>The Athelete</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
+                <Hide>
+                    <Link to="/work/the-athlete">
+                        <motion.img variants={photoAnim} src={athlete} alt="athlete"/>
+                    </Link>
+                </Hide>
             </Movie>
             <Movie>
-                <h2>The Racer</h2>
+                <motion.h2 variants={fade}>The Racer</motion.h2>
                 <div className="line"></div>
-                <Link to="/work/the-racer">
-                    <img src={theracer} alt="theracer"/>
-                </Link>
+                <Hide>
+                    <Link to="/work/the-racer">
+                        <motion.img variants={photoAnim} src={theracer} alt="theracer"/>
+                    </Link>
+                </Hide>
             </Movie>
             <Movie>
-                <h2>Good Times</h2>
+                <motion.h2 variants={fade}>Good Times</motion.h2>
                 <div className="line"></div>
-                <Link to="/work/good-times">
-                    <img src={goodtimes} alt="goodtimes"/>
-                </Link>
+                <Hide>
+                    <Link to="/work/good-times">
+                        <motion.img variants={photoAnim} src={goodtimes} alt="goodtimes"/>
+                    </Link>
+                </Hide>
             </Movie>
         </Work>
     )
@@ -42,15 +54,15 @@ const OurWork = () => {
 const Work = styled(motion.div)`
     min-height: 100vh;
     padding: 5rem 2rem;
+    background-color: #fff;
 `
 const Movie = styled.div`
-    color:#fff;
     h2 {
         padding: 1rem 0;
     }
     .line {
         height: 0.5rem;
-        background-color: #ccc;
+        background-color: #23d997;
         margin: 0 0 3rem;
     }
     img {
@@ -58,5 +70,26 @@ const Movie = styled.div`
         height: 70vh;
         object-fit: cover;
     }
+`
+const Hide = styled.div`
+    overflow: hidden;
+`
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width:100%;
+    height: 100vh;
+    z-index:2;
+    background: #fffebf;
+`
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
 `
 export default OurWork
